a124dbacdb2dac9055a7e154cd5b4d5f9c5c08ee
jeremyben-meir <jsb459@cornell.edu>
2021-11-28 00:31:44 -0500

    reformat init

--- tests/ballot_tests.sol
+++ tests/ballot_tests.sol
@@ -2,7 +2,7 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 import "remix_tests.sol"; // this import is automatically injected by Remix.
-import "../contracts/3_Ballot.sol";
+import "../contracts/ballot.sol";
 
 contract BallotTest {
    
